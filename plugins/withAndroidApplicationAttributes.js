623b8610b63754dba5c76b94805c04789eb0908e
Michael Wood <esiotrot@gmail.com>
2024-01-05 14:18:58 +0200

    Improve withAndroidApplicationAttributes plugin

--- plugins/withAndroidApplicationAttributes.js
+++ plugins/withAndroidApplicationAttributes.js
@@ -1,34 +1,18 @@
-const { withAndroidManifest } = require("@expo/config-plugins");
+const { AndroidConfig, withAndroidManifest } = require("expo/config-plugins");
 
 function addAttributesToApplication(androidManifest, attributes) {
-  const { manifest } = androidManifest;
+  const app = AndroidConfig.Manifest.getMainApplicationOrThrow(androidManifest);
 
-  if (!Array.isArray(manifest["application"])) {
-    console.warn(
-      "withAndroidApplicationAttributes: No manifest.application array?"
-    );
-    return androidManifest;
+  if (app?.$) {
+    app.$ = { ...app.$, ...attributes };
   }
-
-  const application = manifest["application"].find(
-    (item) => item.$["android:name"] === ".MainApplication"
-  );
-  if (!application) {
-    console.warn("withAndroidApplicationAttributes: No .MainApplication?");
-    return androidManifest;
-  }
-
-  application.$ = { ...application.$, ...attributes };
 
   return androidManifest;
 }
 
 module.exports = function withAndroidApplicationAttributes(config, attributes) {
   return withAndroidManifest(config, (config) => {
-    config.modResults = addAttributesToApplication(
-      config.modResults,
-      attributes
-    );
+    config.modResults = addAttributesToApplication(config.modResults, attributes);
     return config;
   });
 };
